7a681b837bfa867c503e81cdbb5d1b20f5b845db
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-01-31 11:15:27 -0700

    Add proper brand colors.

--- src/styles/base/variables.js
+++ src/styles/base/variables.js
@@ -1,9 +1,16 @@
-export const black = '#000'
+export const codGray = '#1A1A1A'
 export const white = '#fff'
+export const bossanova = '#4C2954'
+export const cosmic = '#774175'
+export const revolver = '#281C3D'
+
 
 export const colors = {
-	black,
-	white
+	codGray,
+	white,
+	bossanova,
+	cosmic,
+	revolver
 }
 
 export const bpTiny = 'min-width: 25em' /* 400px */
