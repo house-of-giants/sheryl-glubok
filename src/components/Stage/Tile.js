8c5c709c36511aa8a45b7e0c873f455c153f5714
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-12 21:41:41 -0700

    Refactor to a class component. Adjust logic to better suit our needs.

--- src/components/Stage/Tile.js
+++ src/components/Stage/Tile.js
@@ -1,183 +1,197 @@
-import { useEffect } from 'react'
-import { TweenMax as TM, Power2, Power3, Expo } from 'gsap/all'
+import React from 'react'
+import { TweenMax as TM, Power2 } from 'gsap/all'
 import * as THREE from 'three'
+// import html2canvas from 'html2canvas'
 
-import { clamp, getRatio, wrap, ev } from './utils'
+import { getRatio } from './utils'
 
 import vertexShader from '../../glsl/vertexShader.glsl'
 import fragmentShader from '../../glsl/fragmentShader.glsl'
 
-const Tile = ( el, scene, duration ) => {
-	let isHovering = false
-	let mesh;
-	// const mainImage = el.querySelector( 'img:first-of-type' )
-	// const bgImage = el.querySelector( 'img:last-of-type' )
+class Tile {
+	constructor( el, scene, duration ) {
+		this.scene = scene
+		this.el = el
+		this.duration = duration
 
-	let images = []
-	let sizes = new THREE.Vector2( 0, 0 )
-	let offset = new THREE.Vector2( 0, 0 )
+		// this.mainImage = html2canvas( el )
+		// this.bgImage = html2canvas( el )
+		this.sizes = new THREE.Vector2( 0, 0 )
+		this.offset = new THREE.Vector2( 0, 0 )
 
-	const clock = new THREE.Clock()
+		this.vertexShader = vertexShader
+		this.fragmentShader = fragmentShader
 
-	const mouse = new THREE.Vector2( 0, 0 )
+		this.clock = new THREE.Clock()
 
-	let scroll = 0
-	let prevScroll = 0
-	let delta = 0
+		this.mouse = new THREE.Vector2( 0, 0 )
 
-	// const loader = new THREE.TextureLoader()
+		this.scroll = 0
+		this.prevScroll = 0
+		this.delta = 0
 
-	// const preloadImage = ($el) => {
-	// 	const image = loader.load($el)
-	// 	image.center.set(0.5, 0.5)
-	// 	images.push( image )
-	// }
-
-	// preloadImage( mainImage )
-
-	// if( bgImage ) {
-	// 	preloadImage( bgImage )
-	// }
-
-	// const texture = images[0]
-	// const hoverTexture = images[1]
-
-	// console.log(images)
-
-	let uniforms = {
-		u_alpha: { value: 1 },
-		u_map: { type: 't', value: el },
-		u_ratio: { value: getRatio( sizes, el.querySelector( 'img:first-of-type' ) ) },
-		u_hovermap: { type: 't', value: el },
-		u_hoverratio: { value: getRatio( sizes, el.querySelector( 'img:last-of-type' ) ) },
-		u_mouse: { value: mouse },
-		u_progressHover: { value: 0 },
-		u_progressClick: { value: 0 },
-		u_time: { value: clock.getElapsedTime() },
-		u_res: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+		this.initTile()
+		this.bindEvent()
 	}
 
-	const bindEvent = () => {
-		window.addEventListener( 'mousemove', e => onMouseMove( e ) )
-		window.addEventListener( 'scroll', e => onScroll( e ) )
-		el.addEventListener( 'mouseenter', e => onPointerEnter( e ) )
-		el.addEventListener( 'mouseleave', () => onPointerLeave() )
+	bindEvent() {
+		window.addEventListener( 'resize', () => { this.onResize() } )
+		window.addEventListener( 'mousemove', e => this.onMouseMove( e ) )
+		window.addEventListener( 'scroll', e => this.onScroll( e ) )
+		this.el.addEventListener( 'mouseenter', e => this.onPointerEnter( e ) )
+		this.el.addEventListener( 'mouseleave', () => this.onPointerLeave() )
 	}
 
-	const onPointerEnter = e => {
-		isHovering = true
+	onPointerEnter(e) {
+		this.isHovering = true
 		const bg = e.target.dataset.color
 		document.body.style.setProperty( '--c-bg', bg )
 
-		if ( !mesh ) return
+		if ( !this.mesh ) return
 
-			TM.to( uniforms.u_progressHover, duration, {
-				value: 1,
-				ease: Power2.easeInOut,
-			} )
+		TM.to( this.uniforms.u_progressHover, this.duration, {
+			value: 1,
+			ease: Power2.easeInOut,
+		} )
+
+		console.log(this.uniforms.u_progressHover)
 	}
 
-	const onPointerLeave = () => {
-		if ( !mesh ) return
+	onPointerLeave() {
+		if ( !this.mesh ) return
 
-			TM.to( uniforms.u_progressHover, duration, {
+			TM.to( this.uniforms.u_progressHover, this.duration, {
 				value: 0,
 				ease: Power2.easeInOut,
 				onComplete: () => {
-					isHovering = false
+					this.isHovering = false
 				},
 			} )
 	}
 
-	const onScroll = () => {
-		getBounds()
+	onResize() {
+		this.getBounds()
 
-		TM.set( mesh.position, {
-			x: offset.x,
-			y: offset.y,
+		if ( !this.mesh ) return
+
+		this.mesh.scale.set( this.sizes.x, this.sizes.y, 1 )
+		this.uniforms.u_res.value.set( window.innerWidth, window.innerHeight )
+	}
+
+	onScroll() {
+		this.getBounds()
+
+		TM.set( this.mesh.position, {
+			x: this.offset.x,
+			y: this.offset.y,
 		} )
 	}
 
-	const onMouseMove = e => {
-		TM.to(mouse, 0.5, {
+	onMouseMove( e ) {
+		TM.to( this.mouse, 0.5, {
 			x: e.clientX,
 			y: e.clientY,
 		})
 	}
 
-	const initTile = () => {
-		// const texture = mainImage
-		// const hoverTexture = bgImage
-		getBounds()
+	initTile() {
+		this.getBounds()
 
-		const geometry = new THREE.PlaneBufferGeometry(1, 1, 1, 1)
+		this.uniforms = {
+			u_alpha: { value: 1 },
+			u_mouse: { value: this.mouse },
+			u_progressHover: { value: 0 },
+			u_progressClick: { value: 0 },
+			u_time: { value: this.clock.getElapsedTime() },
+			u_res: { value: new THREE.Vector2(window.innerWidth, window.innerHeight) },
+		}
 
-		const material = new THREE.ShaderMaterial({
-			uniforms,
-			vertexShader,
-			fragmentShader,
+		// this.mainImage.then( canvas => {
+		// 	let texture = new THREE.CanvasTexture( canvas )
+		// 	texture.needsUpdate = true
+
+		// 	let bgTexture = new THREE.CanvasTexture( canvas )
+		// 	bgTexture.needsUpdate = true
+
+		// 	this.uniforms.u_map = { type: 't', value: texture }
+		// 	this.uniforms.u_ratio = { value: getRatio( this.sizes, texture.image.width ) }
+		// 	this.uniforms.u_hovermap = { type: 't', value: bgTexture }
+		// 	this.uniforms.u_hoverratio = { value: getRatio( this.sizes, bgTexture.image.width ) }
+		// } )
+
+		this.geometry = new THREE.PlaneBufferGeometry(1, 1, 1, 1)
+
+		this.material = new THREE.ShaderMaterial({
+			uniforms: this.uniforms,
+			vertexShader: this.vertexShader,
+			fragmentShader: this.fragmentShader,
 			transparent: true,
 			defines: {
-				PI: Math.PI,
+				// PI: Math.PI,
 				PR: window.devicePixelRatio.toFixed(1),
 			},
 		})
 
-		mesh = new THREE.Mesh(geometry, material)
+		// this.mesh = new THREE.Mesh( this.geometry, this.material )
 
-		mesh.position.x = offset.x
-		mesh.position.y = offset.y
+		// this.material = new THREE.MeshBasicMaterial( { wireframe: true } )
+		// this.geometry = new THREE.PlaneGeometry();
 
-		mesh.scale.set( sizes.x, sizes.y, 1 )
+		this.mesh = new THREE.Mesh( this.geometry, this.material )
 
-		scene.add( mesh )
+		// this.cssObject = new THREE.CSS3DObject( this.el )
+
+		// this.cssObject.position = this.mesh.position
+		// this.cssObject.rotation = this.mesh.rotation
+
+		// this.mesh.position.x = this.offset.x
+		// this.mesh.position.y = this.offset.y
+
+		// this.mesh.scale.set( this.sizes.x, this.sizes.y, 1 )
+
+		this.scene.add( this.mesh )
 	}
 
-	const move = () => {
-		getBounds()
+	move() {
+		if( !this.mesh ) return
+		this.getBounds()
 
-		TM.set( mesh.position, {
-			x: offset.x,
-			y: offset.y,
+		TM.set( this.mesh.position, {
+			x: this.offset.x,
+			y: this.offset.y,
 		} )
 
-		TM.to( mesh.scale, 0.3, {
-			x: sizes.x - delta,
-			y: sizes.y - delta,
+		TM.to( this.mesh.scale, 0.3, {
+			x: this.sizes.x - this.delta,
+			y: this.sizes.y - this.delta,
 			z: 1,
 		} )
 	}
 
-	const getBounds = () => {
-		const { width, height, left, top } = el.getBoundingClientRect()
+	update() {
+		this.delta = Math.abs( ( this.scroll - this.prevScroll ) * 2000 )
 
-		if ( !sizes.equals( new THREE.Vector2( width, height ) ) ) {
-			sizes.set( width, height )
-		}
+		if ( !this.mesh ) return
 
-		if ( !offset.equals( new THREE.Vector2( left - window.innerWidth / 2 + width / 2, -top + window.innerHeight / 2 - height / 2 ) ) ) {
-			offset.set( left - window.innerWidth / 2 + width / 2, -top + window.innerHeight / 2 - height / 2 )
-		}
+		this.move()
+
+		this.prevScroll = this.scroll
+
+		if ( !this.isHovering ) return
+		this.uniforms.u_time.value += this.clock.getDelta()
 	}
 
-	const update = () => {
-		delta = Math.abs((scroll - prevScroll) * 2000)
+	getBounds() {
+		const { width, height, left, top } = this.el.getBoundingClientRect()
 
-		if ( !mesh ) return
+		if ( !this.sizes.equals( new THREE.Vector2( width, height ) ) ) {
+			this.sizes.set( width, height )
+		}
 
-			move()
-
-		prevScroll = scroll
-
-		if (!isHovering) return
-			uniforms.u_time.value += clock.getDelta()
+		if ( !this.offset.equals( new THREE.Vector2( left - window.innerWidth / 2 + width / 2, -top + window.innerHeight / 2 - height / 2 ) ) ) {
+			this.offset.set( left - window.innerWidth / 2 + width / 2, -top + window.innerHeight / 2 - height / 2 )
+		}
 	}
-
-	initTile()
-
-	bindEvent()
-
-	return( null )
 }
 
 export default Tile
