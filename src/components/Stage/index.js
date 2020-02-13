dd6e3b8aa8431652f01ab28ea909c0d0f7e58fbe
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-12 21:42:54 -0700

    Pass individual films to the Stage component instead of using them on the index page.

--- src/components/Stage/index.js
+++ src/components/Stage/index.js
@@ -2,25 +2,38 @@ import React, { useEffect, useRef } from 'react'
 import * as THREE from 'three'
 
 import { StyledStage } from './StyledStage'
+
+import Fishbowl from '../Films/Fishbowl'
+import LilyNRose from '../Films/LilyNRose'
+import Sonnet98 from '../Films/Sonnet98'
+import Thaw from '../Films/Thaw'
+import Alexia from '../Films/Alexia'
 import Scene from './Scene'
 
-export const Canvas = () => {
-	const canvasRef = useRef(null)
+const Stage = () =>  {
+	const canvasRef = useRef( null )
+	const fishbowlRef = useRef( null )
+	const lilynroseRef = useRef( null )
+	const sonnet98Ref = useRef( null )
+	const thawRef = useRef( null )
+	const alexiaRef = useRef( null )
 
 	useEffect( () => {
 		const canvas = canvasRef.current
-		const scene = new Scene( canvas )
+		const els = [ fishbowlRef.current ] 
+		const scene = new Scene( canvas, els )
 	}, [] )
 
 	return (
-		<canvas ref={ canvasRef } id="stage"></canvas>
+		<StyledStage>
+			<Fishbowl ref={ fishbowlRef } />
+			<LilyNRose ref={ lilynroseRef } />
+			<Sonnet98 ref={ sonnet98Ref } />
+			<Thaw ref={ thawRef } />
+			<Alexia ref={ alexiaRef } />
+			<canvas ref={ canvasRef } id="stage"></canvas>
+		</StyledStage>
 	)
 }
-
-const Stage = () => (
-	<StyledStage>
-		<Canvas />
-	</StyledStage>
-)
 
 export default Stage
