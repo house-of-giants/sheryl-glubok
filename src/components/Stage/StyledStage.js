1186f3b6840ae89f0797f2052e6b4be04424bded
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-12 21:42:07 -0700

    Reduce z-index to appear beneath SVG titles. Add mix blend mode for now.

--- src/components/Stage/StyledStage.js
+++ src/components/Stage/StyledStage.js
@@ -1,13 +1,14 @@
 import styled from 'styled-components'
 
 export const StyledStage = styled.div`
-	canvas {
+	& canvas {
 		position: fixed;
 		left: 0;
+		mix-blend-mode: luminosity;
 		pointer-events: none;
 		top: 0;
 		width: 100%;
 		height: 100vh;
-		z-index: 9;
+		z-index: 0;
 	}
 `
