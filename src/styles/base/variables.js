d79f6df66ce739e72521bd8a7d8ce1526eac9318
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-17 14:47:31 -0700

    Add xl breakpoint, ensure parallax-y hover does not overflow container causing scroll.

--- src/styles/base/variables.js
+++ src/styles/base/variables.js
@@ -19,13 +19,15 @@ export const bpTiny = 'min-width: 25em' /* 400px */
 export const bpSmall = 'min-width: 40.625em' /* 650px */
 export const bpMedium = 'min-width: 53.125em' /* 850px */
 export const bpLarge = 'min-width: 62.5em' /* 1000px */
+export const bpXLarge = 'min-width: 90em' /* 1440px */
 
 
 export const mq = {
 	bpTiny,
 	bpSmall,
 	bpMedium,
-	bpLarge
+	bpLarge,
+	bpXLarge,
 }
 
 export const containerWidth = '85.333em' /* 1024px */
--- src/styles/global/layout.js
+++ src/styles/global/layout.js
@@ -1,8 +1,13 @@
 import styled from 'styled-components'
-import { container } from '../base/variables'
+import { container, mq } from '../base/variables'
 
 export const StyledContainer = styled.div`
-  margin: 0 auto;
-  max-width: ${ container.width };
-  padding: 0 1rem;
+	margin: 0 auto;
+	max-width: ${ container.width };
+	overflow-x: hidden;
+	padding: 0 1rem;
+
+	@media (${mq.bpXLarge}) {
+		overflow-x: visible;
+	}
 `
