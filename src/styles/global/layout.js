77d13ad4f20bfe496df9816636d7b9bca003c18f
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-18 14:10:26 -0700

    Add narrow variation to added wrapper.

--- src/styles/global/layout.js
+++ src/styles/global/layout.js
@@ -2,7 +2,11 @@ import styled from 'styled-components'
 import { container } from '../base/variables'
 
 export const StyledContainer = styled.div`
-  margin: 0 auto;
-  max-width: ${ container.width };
-  padding: 0 1rem;
+	margin: 0 auto;
+	max-width: ${ container.width };
+	padding: 0 1rem;
+
+	& .wrap {
+		max-width: ${ container.widthNarrow };
+	}
 `
