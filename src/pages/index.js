6e7d81ebe04c7a914fdb4c3003faeac06424b093
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-18 11:45:07 -0700

    Add Styled About section for the home page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,17 +2,20 @@ import React from 'react'
 
 import Layout from '../theme/layout'
 import Hero from '../components/Hero'
+import NavLogo from '../components/Nav/NavLogo'
 import Fishbowl from '../components/Films/Fishbowl'
 import LilyNRose from '../components/Films/LilyNRose'
 import Sonnet98 from '../components/Films/Sonnet98'
 import Thaw from '../components/Films/Thaw'
 import Alexia from '../components/Films/Alexia'
+import About from '../components/About'
 
 import { StyledContainer } from '../styles/global/layout'
 
 export default () => (
 	<Layout>
 		<Hero />
+		<NavLogo />
 		<StyledContainer>
 			<Fishbowl />
 			<LilyNRose />
@@ -20,5 +23,6 @@ export default () => (
 			<Thaw />
 			<Alexia />
 		</StyledContainer>
+		<About />
 	</Layout>
 )
