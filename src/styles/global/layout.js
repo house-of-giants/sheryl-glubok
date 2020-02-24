0fd70f88f624db6b8e5cfd90ab3df579c6e31add
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-24 14:29:01 -0700

    Add blog specific container.

--- src/styles/global/layout.js
+++ src/styles/global/layout.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-import { container, mq } from '../base/variables'
+import { container, mq, colors } from '../base/variables'
 
 export const StyledContainer = styled.div`
 	margin: 0 auto;
@@ -8,5 +8,20 @@ export const StyledContainer = styled.div`
 
 	& .wrap {
 		max-width: ${ container.widthNarrow };
+	}
+`
+
+export const StyledBlogContainer = styled.section`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+
+	& .heading {
+		display: grid;
+		grid-column: 1 / span 2;
+		grid-template-columns: repeat(2, 1fr);
+	}
+
+	& svg {
+		fill ${ colors.white };
 	}
 `
