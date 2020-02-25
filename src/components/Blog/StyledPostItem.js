9ad93a28d5372f960e73fc4dc17b13f141723fdb
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-25 09:29:20 -0700

    Add layout and styles for the Post items.

--- src/components/Blog/StyledPostItem.js
+++ src/components/Blog/StyledPostItem.js
@@ -1,13 +1,45 @@
 import styled from 'styled-components'
 
-import { colors } from '../../styles/base/variables'
+import { colors, mq } from '../../styles/base/variables'
 
 const StyledPostItem = styled.article`
 	display: flex;
 	flex-direction: column;
 
+	@media (${ mq.bpLarge }) {
+		&:first-child,
+		&:last-child,
+		&:nth-child(4),
+		&:nth-child(5) {
+			& .image::before {
+				padding-top: calc((81 / 77) * 100%);
+			}
+		}
+
+		&:nth-child(4n+1) {
+			transform: translateY(-1.5rem);
+		}
+
+		&:nth-child(4n+2) {
+			align-self: flex-end;
+		}
+
+		&:nth-child(4n+3) {
+			align-self: flex-start;
+			transform: translate(7vw, 1.5rem)
+		}
+
+		&:nth-child(4n+4) {
+			transform: translate(7vw, 4rem);
+		}
+	}
+
 	& a {
 		color: ${colors.white};
+	}
+
+	& .title {
+		margin: 0.25rem 0;
 	}
 
 	& .date {
@@ -27,9 +59,25 @@ const StyledPostItem = styled.article`
 		}
 	}
 
+	& .image {
+		position: relative;
+
+		&::before {
+			content: "";
+			display: block;
+			padding-top: calc((46 / 77) * 100%);
+			width: 100%;
+		}
+	}
+
 	& img {
+		bottom: 0;
 		height: 100%;
+		left: 0;
 		object-fit: cover;
+		position: absolute;
+		right: 0;
+		top: 0;
 		width: 100%;
 	}
 `
