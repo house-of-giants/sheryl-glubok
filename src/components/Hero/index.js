275942df59b7d9aac70d054a0c1d524c3f7bba26
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-26 17:39:07 -0700

    Give the logo the layer style treatment.

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -5,14 +5,18 @@ import { StyledHero } from './StyledHero'
 
 const Hero = () => (
 	<StyledHero>
-		<img src="https://images.unsplash.com/photo-1534736487074-a559cde7d1aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80g" alt=""/>
-		<img src="https://images.unsplash.com/photo-1534736487074-a559cde7d1aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80g" alt=""/>
-		<img src="https://images.unsplash.com/photo-1534736487074-a559cde7d1aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80g" alt=""/>
-		<img src="https://images.unsplash.com/photo-1534736487074-a559cde7d1aa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2089&q=80g" alt=""/>
-		<div className="content">
-			<Logo />
-			<h1 className="screen-reader-text">Sheryl Glubok</h1>
-			<p>Writer, Director, Storyteller</p>
+		<img src="/sheryl-car.jpg" alt=""/>
+		<img src="/sheryl-car.jpg" alt=""/>
+		<img src="/sheryl-car.jpg" alt=""/>
+		<img src="/sheryl-car.jpg" alt=""/>
+		<div className="wrap">
+			<div className="content -difference">
+				<Logo />
+				<h1 className="screen-reader-text">Sheryl Glubok</h1>
+			</div>
+			<div className="content">
+				<p>Writer, Director, Storyteller</p>
+			</div>
 		</div>
 	</StyledHero>
 )
