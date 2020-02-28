7d665e9164e1af7718a97b939a2a9a17dba97a64
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-02-28 13:14:11 -0700

    Add basic page transitions.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,46 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Link } from 'gatsby'
 import { AnimatePresence, motion, useCycle } from 'framer-motion'
-import { TweenMax } from 'gsap'
 
 import { StyledContainer } from '../../styles/global/layout'
-import { StyledNav, StyledNavActivator } from './StyledNav'
+import { StyledNav, StyledNavActivator, animWrap, animList, animItem } from './StyledNav'
 
 const Nav = () => {
 	const [ isToggled, setToggled ] = useCycle(false, true)
 
 	const toggleNav = () => setToggled(!isToggled)
-
-	const wrap = {
-		visible: { opacity: 1 },
-		hidden: { opacity: 0 }
-	}
-
-	const list = {
-		visible: {
-			transition: { staggerChildren: 0.07, delayChildren: 0.2 }
-		},
-		hidden: {
-			transition: { staggerChildren: 0.05, staggerDirection: -1 }
-		}
-	}
-
-	const item = {
-		visible: {
-			y: 0,
-			opacity: 1,
-			transition: {
-				y: { stiffness: 1000, velocity: -100 }
-			}
-		},
-		hidden: {
-			y: 50,
-			opacity: 0,
-			transition: {
-				y: { stiffness: 1000 }
-			}
-		}
-	}
 
 	const pages = [ 'Home', 'Films', 'About', 'Blog' ]
 
@@ -48,13 +16,13 @@ const Nav = () => {
 		<div>
 			<StyledContainer>
 				<StyledNavActivator onClick={ toggleNav } className={isToggled ? 'is-active' : ''} >Menu</StyledNavActivator>
-				<AnimatePresence>
+				<AnimatePresence exitBeforeEnter>
 					{ isToggled &&
-						<StyledNav variants={wrap} initial="hidden" animate="visible" exit="hidden">
-							<motion.ul variants={list}>
+						<StyledNav variants={animWrap} initial="hidden" animate="visible" exit="hidden">
+							<motion.ul variants={animList}>
 								{ pages.map( (page, i) => (
-									<motion.li variants={item} i={i} key={page}>
-										<Link to={`/${page.toLowerCase()}`}>{page}</Link>
+									<motion.li variants={animItem} i={i} key={page}>
+										<Link to={`/${page === 'Home' ? '' : page.toLowerCase()}`}>{page}</Link>
 									</motion.li>
 								) ) }
 							</motion.ul>
