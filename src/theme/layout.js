981d3e70b615f199df7c143f377cc2a6c5885aeb
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-03 18:06:02 -0700

    Add prop for the page title if desired.

--- src/theme/layout.js
+++ src/theme/layout.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import { createGlobalStyle } from 'styled-components'
+import Helmet from 'react-helmet'
 
 import globalStyles from '../styles/styles'
 
 import Nav from '../components/Nav'
 import Footer from '../components/Footer'
+import Head from '../components/Head'
 
 const GlobalStyle = createGlobalStyle`
 	${globalStyles}
 `
 
-const Layout = ({ children }) => (
+const Layout = ({ pageTitle, children }) => (
 	<div className='app'>
 		<GlobalStyle />
+		<Helmet>
+			<title>Sheryl Glubok | {pageTitle ? `${pageTitle}` : 'Writer, Director, Storyteller'}</title>
+		</Helmet>
+		<Head />
 		<Nav />
 		{children}
 		<Footer />
