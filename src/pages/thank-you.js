70eeb7643b4d44b65edf2e7a2ac710d15983464c
Kelly Bleck <kellydbleck@gmail.com>
2020-03-04 11:32:23 -0700

    Add Contact Form

---
+++ src/pages/thank-you.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+import Layout from '../theme/layout'
+import NavLogo from '../components/Nav/NavLogo'
+import Hero from '../components/Hero'
+
+import { StyledContainer } from '../styles/global/layout'
+import { animPageDefault } from '../utils/animationDefs'
+import { AnimatePresence, motion } from 'framer-motion'
+
+export default () => (
+	<Layout pageMeta={{ title: 'Thank You' }}>
+		<NavLogo />
+		<StyledContainer>
+			<Hero />
+		</StyledContainer>
+		<StyledContainer narrow>
+			<AnimatePresence>
+				<motion.div className="wrap" variants={animPageDefault} initial="in" animate="normal" exit="out" >
+					<h2>Thank You</h2>
+					<p>Thank you for getting in touch! Sheryl will be in contact as soon as she can.</p>
+				</motion.div>
+			</AnimatePresence>
+		</StyledContainer>
+	</Layout>
+)
