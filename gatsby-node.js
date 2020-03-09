f6c48c7fcbe5f2ecaff4ca791d9e3aad95f34dde
Kelly Bleck <kellydbleck@gmail.com>
2020-03-09 14:34:26 -0600

    Add pagination to Blog Listing

--- gatsby-node.js
+++ gatsby-node.js
@@ -20,10 +20,51 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 
 exports.createPages = async ({ graphql, actions }) => {
 	const { createPage } = actions
+	const blogResults = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        filter: { fields: { layout: { eq: "blog" } } }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            fields {
+              slug
+              layout
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  if (blogResults.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query.`)
+    return
+  }
+
+  const posts = blogResults.data.allMarkdownRemark.edges
+  const postsPerPage = 4
+  const numPages = Math.ceil(posts.length / postsPerPage)
+  Array.from({ length: numPages }).forEach((_, i) => {
+    createPage({
+      path: i === 0 ? `/blog` : `/blog/${i + 1}`,
+      component: path.resolve("src/templates/blog-listing.js"),
+      context: {
+        limit: postsPerPage,
+        skip: i * postsPerPage,
+        numPages,
+        currentPage: i + 1,
+      },
+    })
+  })
+
 	const result = await graphql(`
     {
       allMarkdownRemark(
         sort: { order: DESC, fields: [frontmatter___date] }
+        filter: { fields: { layout: { eq: "film" } } }
         limit: 1000
       ) {
         edges {
