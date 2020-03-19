98c265cd824b88ef77d4f306911628ad70e706f3
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-19 11:40:55 -0600

    Add 404 page.

---
+++ src/pages/404.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Layout from '../theme/layout'
+import NavLogo from '../components/Nav/NavLogo'
+
+import { StyledContainer } from '../styles/global/layout'
+import { animPageDefault } from '../utils/animationDefs'
+import { AnimatePresence, motion } from 'framer-motion'
+
+export default () => (
+	<Layout pageMeta={{ title: 'Thank You' }}>
+		<NavLogo />
+		<StyledContainer narrow>
+			<AnimatePresence exitBeforeEnter>
+				<motion.div className="wrap" variants={animPageDefault} initial="in" animate="normal" exit="out" >
+					<div className="blockquote"><h1>404</h1></div>
+					<p>Looks like the page you're looking for doesn't exist. Try navigating to another page.</p>
+				</motion.div>
+			</AnimatePresence>
+		</StyledContainer>
+	</Layout>
+)
