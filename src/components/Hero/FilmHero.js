9466d447821ed00fbe7501aa88b3f79294fc256b
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-19 12:35:58 -0600

    Add check to determine if a film has a video or not, display the play button appropriately.

--- src/components/Hero/FilmHero.js
+++ src/components/Hero/FilmHero.js
@@ -4,16 +4,19 @@ import PropTypes from 'prop-types'
 import Button from '../Button'
 import StyledFilmHero from './StyledFilmHero'
 
-const FilmHero = ({ isVideo, showVideo, thumbnail }) => (
-	<StyledFilmHero onClick={() => showVideo( ! isVideo )}>
+const FilmHero = ({ hasVideo, isVideo, showVideo, thumbnail }) => (
+	<StyledFilmHero onClick={() => showVideo( ! isVideo )} hasVideo={hasVideo}>
 		<img src={thumbnail} alt="" />
-		<Button isButton>
-			<span className="screen-reader-text">Play Video</span>
-		</Button>
+		{ hasVideo &&
+			<Button isButton>
+				<span className="screen-reader-text">Play Video</span>
+			</Button>
+		}
 	</StyledFilmHero>
 )
 
 FilmHero.propTypes ={
+	hasVideo: PropTypes.bool.isRequired,
 	isVideo: PropTypes.bool.isRequired,
 	showVideo: PropTypes.func.isRequired,
 	thumbnail: PropTypes.string.isRequired,
