19d330959a465fd8ba7309fc87bb689486feccdc
Kelly Bleck <kellydbleck@gmail.com>
2020-03-26 13:36:15 -0600

    Move home About section to About page

--- src/components/About/AboutCTA/index.js
+++ src/components/About/AboutCTA/index.js
@@ -1,18 +1,37 @@
 import React from 'react'
-
-// import YAMLData from "../../../content/home.md"
+import PropTypes from 'prop-types'
 
 import { Logo, AboutCTATitle } from '../../SVG'
 import { StyledContainer } from '../../../styles/global/layout'
 import { StyledAboutCTA } from './StyledAboutCTA'
 import Button from '../../Button'
 
-const AboutCTA = () => (
-	<StyledAboutCTA>
-		<StyledContainer>
-			<Logo />
-		</StyledContainer>
-	</StyledAboutCTA>
-)
+const AboutCTA = (props) => {
+	const { column_one_img, column_two_img, column_three_img, body } = props.data[0]
+
+	return (
+		<StyledAboutCTA>
+			<StyledContainer>
+				<Logo />
+				<div className="strip">
+					<img src={column_one_img} alt=""/>
+					<img src={column_two_img} alt=""/>
+					<img src={column_three_img} alt=""/>
+				</div>
+				<div className="content">
+					<AboutCTATitle />
+					<div className="blurb">
+						<p>{body}</p>
+						<Button to="/about">Learn More About Sheryl</Button>
+					</div>
+				</div>
+			</StyledContainer>
+		</StyledAboutCTA>
+	)
+}
+
+AboutCTA.propTypes ={
+	data: PropTypes.array.isRequired
+}
 
 export default AboutCTA
