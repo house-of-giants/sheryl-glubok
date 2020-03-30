6337b5371bcc560098719fe3f4c3312d7f500483
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-03-30 15:25:36 -0600

    Leverage hooks to set the thumbnail.

--- src/components/Content/ResponsiveImg.js
+++ src/components/Content/ResponsiveImg.js
@@ -1,10 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const ResponsiveImg = ( { className, src, alt = "", params = "ar_16:9,c_fill,g_face/c_scale,w_auto/c_limit,w_1000" } ) => {
-	const baseURL = new URL(window.location.href)
-	const { protocol, host } = baseURL
-	const formattedURL = host === 'localhost:8000' ? 'https://compassionate-lamarr-33dd20.netlify.com' : `${protocol}//${host}`
-	const thumb = src ? `${formattedURL}${src}` : 'https://source.unsplash.com/user/claudiotesta/1350x478'
+	const [thumb, setThumb] = useState('')
+
+	useEffect( () => {
+		const baseURL = new URL(window.location.href)
+		const { protocol, host } = baseURL
+		const formattedURL = host === 'localhost:8000' ? 'https://compassionate-lamarr-33dd20.netlify.com' : `${protocol}//${host}`
+		setThumb( src ? `${formattedURL}${src}` : 'https://source.unsplash.com/user/claudiotesta/1350x478' )
+	}, [src] )
 	return (
 		<img
 			className={className}
